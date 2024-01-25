a75f6ff23fde98e642085de1a5c723284ecb159c
Stobles <ilovespnjensen@inbox.ru>
2024-01-26 00:05:06 +0300

    fix: post layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default async function Home() {
         <h1 className="font-bold text-3xl md:text-4xl">Записи</h1>
       )}
       <div className="grid grid-cols-1 lg:grid-cols-3 lg:grid-rows-3 gap-y-4 lg:gap-x-4 py-6">
-        <div className="grid col-start-1 col-end-3 row-start-1 row-end-4">
+      <div className="grid col-start-1 col-end-3 row-start-1 row-end-4">
           {session?.user ? (
               <Suspense fallback={<div>Загрузка...</div>}>
                 {/* @ts-expect-error server component */}
