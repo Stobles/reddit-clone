2c6078476507657473d7218cd45ef57a5f2d8c20
Stobles <ilovespnjensen@inbox.ru>
2024-01-25 22:29:36 +0300

    fix: add button code

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -46,7 +46,8 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         disabled={isLoading}
         {...props}
       >
-        {isLoading ? <Loader2 className="h-4 w-4 animate-spin" /> : children}
+        {isLoading ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : null}
+        {children}
       </button>
     );
   }
